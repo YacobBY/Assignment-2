934a22255a21a7656a4a497b25d184c9f3d21e1e
Yacob <yacob101@gmail.com>
2018-10-04 11:38:28 +0200

    selectsort extra fix

--- high-score-student/src/main/java/nl/hva/ict/ds/SelectionSort.java
+++ high-score-student/src/main/java/nl/hva/ict/ds/SelectionSort.java
@@ -3,8 +3,8 @@
 package nl.hva.ict.ds;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
-import java.util.*;
 
 public class SelectionSort implements HighScoreList {
     private List<Player> players = new ArrayList<>();
@@ -16,12 +16,16 @@ public class SelectionSort implements HighScoreList {
         int i = 0;
         System.out.println(players.size());
 
-        for (int j = 0; j < players.size()-1; j++) {
+        for (int j = 0; j < players.size() - 1; j++) {
             {
-                if (i == (players.size()-1)){return;}
-                //Als je niet bij het einde van de lijst bent **EN** de huidige highscore groter is dan die van de volgende player
-                if ( (players.get(i).getHighScore() < players.get(i + 1).getHighScore())) {
-                    while (i < players.size()-1 && players.get(i).getHighScore() < players.get(i + 1).getHighScore()) {
+                if (i == (players.size() - 1)) {
+                    return;
+                }
+                //Als player highscore kleiner is dan de volgende highscore
+                if ((players.get(i).getHighScore() < players.get(i + 1).getHighScore())) {
+                    //Als je niet bij het einde van de lijst bent **EN** de volgende highscore kleiner is dan de highscore erna
+                    i++;
+                    while (i < players.size() - 1 && players.get(i).getHighScore() < players.get(i + 1).getHighScore()) {
                         i++;
                     }
                     //maakt kopie van volgende player
@@ -36,27 +40,28 @@ public class SelectionSort implements HighScoreList {
             }
         }
     }
-        @Override
-        public List<Player> getHighScores ( int numberOfHighScores){
-            //als er meer highscores worden opgevraagd dan er zijn
-            if (numberOfHighScores > players.size()) {
-                //print dit uit
-                System.out.println("\n**You have demanded " + (numberOfHighScores - players.size()) + " more highscores than availible**");
-                //en voer de functie daarna uit met het maximale aantal highscores mogelijk
-                return getHighScores(players.size());
-            }
-            //Omdat het uigesloten is dat numberOfHighscores nu nog groter is dan Players kan dit weggelaten worden.
-            return players.subList(0, numberOfHighScores);
-        }
 
-        @Override
-        public List<Player> findPlayer (String firstName, String lastName){
-            List<Player> matchedPlayers = new ArrayList<>();
-            for (Player player : players) {
-                if (player.getFirstName().equals(firstName)) {
-                    matchedPlayers.add(player);
-                }
-            }
-            return matchedPlayers;
+    @Override
+    public List<Player> getHighScores(int numberOfHighScores) {
+        //als er meer highscores worden opgevraagd dan er zijn
+        if (numberOfHighScores > players.size()) {
+            //print dit uit
+            System.out.println("\n**You have demanded " + (numberOfHighScores - players.size()) + " more highscores than availible**");
+            //en voer de functie daarna uit met het maximale aantal highscores mogelijk
+            return getHighScores(players.size());
         }
+        //Omdat het uigesloten is dat numberOfHighscores nu nog groter is dan Players kan dit weggelaten worden.
+        return players.subList(0, numberOfHighScores);
     }
+
+    @Override
+    public List<Player> findPlayer(String firstName, String lastName) {
+        List<Player> matchedPlayers = new ArrayList<>();
+        for (Player player : players) {
+            if (player.getFirstName().equals(firstName)) {
+                matchedPlayers.add(player);
+            }
+        }
+        return matchedPlayers;
+    }
+}
