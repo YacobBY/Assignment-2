db32b000b1f049877174c60b51144db5bbd0a9a3
Unknown <yacob101@gmail.com>
2018-10-06 00:03:09 +0200

    COMMEEEEEEEEEEEEEEENTS

--- high-score-student/src/main/java/nl/hva/ict/ds/SelectionSortHighScores.java
+++ high-score-student/src/main/java/nl/hva/ict/ds/SelectionSortHighScores.java
@@ -11,25 +11,32 @@ public class SelectionSortHighScores implements HighScoreList {
 
     @Override
     public void add(Player player) {
-
+//voeg een player toe aan het einde van de lijst
         players.add(player);
+        //als de lijst slechts een player bevat hoef je niet te sorteren dus ben je klaar
         if (players.size() <= 1) {
             return;
         }
+        //loopt van begin tot de totale grootte van de lijst
         for (int i = 0; i < players.size()-1; i++) {
+            //zet de tijdelijke indexen gelijk aan de begin index
             int loopIndex = i;
             int holderIndex = i;
+            //zolang de huidige vergelijk plaats kleiner is dan de maximum grootte min een
             while ((loopIndex) < players.size()-1) {
+                //vergelijk de tijdelijke index met de volgende index
                 if (players.get(holderIndex).getHighScore() < players.get(loopIndex+1).getHighScore()) {
+                    //als de tijdelijke index kleiner is dan de volgende index is de volgende index de tijdelijke index
                     holderIndex = loopIndex+1;
-
                 }
                 //verhoog de huidige loop index met 1 zodat de while de volgende index checkt
                 loopIndex++;
             }
+            //als er een grotere waarde dan de eerste index is gevonden wordt de grootste waarde verwisseld met de eerste
+            if (holderIndex > i){
             Player temp = players.get(holderIndex);
             players.set(holderIndex, players.get(i));
-            players.set(i, temp);
+            players.set(i, temp);}
         }
 
     }
